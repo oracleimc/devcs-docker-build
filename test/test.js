22f7befba952f5d9c65b2797e9d277ac5e05c570
Alper Ozisik <alper.ozisik@oracle.com>
2019-08-21 21:55:05 +0300

    linting tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,24 @@
+/* eslint-disable padded-blocks */
+/* global describe, it */
 // Import the dependencies for testing
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../');
+
 chai.use(chaiHttp);
 chai.should();
-describe("Hello World", () => {
-    describe("GET /", () => {
-        it("should get \"Hello World!\" response", (done) => {
-             chai.request(app)
-                 .get('/')
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.text.should.be.a('string');
-                     res.text.should.be.equal("Hello World!")
-                     done();
-                  });
-         });
+describe('Hello World', () => {
+    describe('GET /', () => {
+        it('should get "Hello World!" response', (done) => {
+            chai.request(app).
+                get('/').
+                end((err, res) => {
+                    console.error(err);
+                    res.should.have.status(200);
+                    res.text.should.be.a('string');
+                    res.text.should.be.equal('Hello World!')
+                    done();
+                });
+        });
     });
 });
